ec2dc2982a172f94bd9d337d88b6d7be876dd7e6
Axurit <hjunca@skalv-studio.fr>
2023-06-15 14:00:07 +0200

    dropdown menu

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { Favicon, Logo, LogoMenu, LogoCross } from "../SVGs";
 
+import stakeLab from "../../assets/images/stakelab.svg"
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -23,11 +25,47 @@ const Navbar = () => {
 
         {/* Desktop */}
         <div className={`Navbar__menu--desktop ${isMenuOpen ? 'open' : ''}`}>
-          <a href="#" className="Navbar__link--white">Product</a>
+          <div className="Navbar__menu--desktop__item">
+            <a href="#" className="Navbar__link--white">Product</a>
+            <div className="Navbar__dropdown">
+              <div className="Navbar__dropdown__item">
+                <img src={stakeLab} alt="StakeLab icon" className="Navbar__dropdown__item__img" />
+                <div className="Navbar__links__wrapper">
+                  <p className="Navbar__link--white">StakeLab</p>
+                  <p className="Navbar__link--white--mini">Live</p>
+                </div>
+              </div>
+              {/* Ajoutez plus d'items ici */}
+            </div>
+          </div>
           <a href="#" className="Navbar__link--white">Network</a>
           <a href="#" className="Navbar__link--white">About</a>
           <a href="#" className="Navbar__link--white">FAQ</a>
           <button className="btn--small btn__primary">Stake</button>
+        </div>
+        <div className="Navbar__dropdown">
+          <div className="Navbar__dropdown__item">
+            <img src="path/to/image" alt="Item 1" />
+            <div>
+              <h3>Item Title</h3>
+              <p>Item Subtitle</p>
+            </div>
+          </div>
+          <div className="Navbar__dropdown__item">
+            <img src="path/to/image" alt="Item 1" />
+            <div>
+              <h3>Item Title</h3>
+              <p>Item Subtitle</p>
+            </div>
+          </div>
+          <div className="Navbar__dropdown__item">
+            <img src="path/to/image" alt="Item 1" />
+            <div>
+              <h3>Item Title</h3>
+              <p>Item Subtitle</p>
+            </div>
+          </div>
+          {/* Ajoutez plus d'items ici */}
         </div>
       </div>
     </nav>
