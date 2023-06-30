620f6f6cd3a8e30eb3f6d21e07bec1b266bbf9de
Axurit <hjunca@skalv-studio.fr>
2023-06-30 14:21:05 +0200

    add link on twitter team member account

--- src/pages/About/Team.tsx
+++ src/pages/About/Team.tsx
@@ -11,24 +11,30 @@ export const Team: React.FC = () => {
         <div className="Team__content__member">
           <img src={Member1} alt="Draw of a member of the team" className="Team__content__member__draw" />
           <div className="Team__content__member__twitter">
-            <a href="#" className="Team__content__member__twitter__link link__white">@zdeadex</a>
-            <LogoTwitter className="Team__content__member__twitter__icon icon__link"/>
+            <a href="https://twitter.com/zdeadex" target="_blank" className="Team__content__member__twitter__link link__white">
+              @zdeadex
+              <LogoTwitter className="Team__content__member__twitter__icon icon__link"/>
+            </a>
           </div>
           <p className="Team__content__member__job">Founder and Chief Executive Officer</p>
         </div>
         <div className="Team__content__member">
           <img src={Member2} alt="Draw of a member of the team" className="Team__content__member__draw" />
           <div className="Team__content__member__twitter">
-            <a href="#" className="Team__content__member__twitter__link link__white">@charles_judith</a>
-            <LogoTwitter className="Team__content__member__twitter__icon icon__link"/>
+            <a href="https://twitter.com/charles_judith" target="_blank" className="Team__content__member__twitter__link link__white">
+              @charles_judith
+              <LogoTwitter className="Team__content__member__twitter__icon icon__link"/>
+            </a>
           </div>
           <p className="Team__content__member__job">Chief Infrastructure Officer</p>
         </div>
         <div className="Team__content__member">
           <img src={Member3} alt="Draw of a member of the team" className="Team__content__member__draw" />
           <div className="Team__content__member__twitter">
-            <a href="#" className="Team__content__member__twitter__link link__white">@dianeyes</a>
-            <LogoTwitter className="Team__content__member__twitter__icon icon__link"/>
+            <a href="https://twitter.com/dianeyes" target="_blank" className="Team__content__member__twitter__link link__white">
+              @dianeyes
+              <LogoTwitter className="Team__content__member__twitter__icon icon__link"/>
+            </a>
           </div>
           <p className="Team__content__member__job">Chief Design Officer</p>
         </div>
