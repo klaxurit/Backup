1d3ef33c128372606a2851ffe59e184d9008ec68
Axurit <hjunca@skalv-studio.fr>
2023-06-30 14:21:46 +0200

    fix build error

--- src/pages/FAQ/index.tsx
+++ src/pages/FAQ/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import { LogoArrowLink } from "../../components/SVGs";
 
 const FAQ: React.FC = () => {
