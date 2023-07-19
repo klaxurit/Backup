21fc5c5e557ccf1d604fcce079c9e7244177353b
Axurit <hjunca@skalv-studio.fr>
2023-07-19 15:05:24 +0200

    add webm format video

--- src/pages/Landing/MainIllustration.tsx
+++ src/pages/Landing/MainIllustration.tsx
@@ -1,10 +1,14 @@
 import MainIllustrationSVG from "../../assets/images/Logo.mp4";
+// import MainIllustrationOGV from "../../assets/images/Logo.ogg";
+import MainIllustrationWEBM from "../../assets/images/Logo.webm";
 
 export const MainIllustration: React.FC = () => {
   return (
     <div className="main-illustration">
       <video playsInline autoPlay loop muted className="main-video">
-        <source src={MainIllustrationSVG} />
+        <source src={MainIllustrationSVG} type="video/mp4" />
+        {/* <source src={MainIllustrationOGV} type="video/ogv" /> */}
+        <source src={MainIllustrationWEBM} type="video/webm" />
       </video>
     </div>
   );
