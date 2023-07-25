3daa42c0378dcde6118f29be79dcf6dfe379cdaf
Axurit <hjunca@skalv-studio.fr>
2023-07-25 17:41:56 +0200

    resolve font demibold issue

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -15,19 +15,19 @@ const Navbar = () => {
     {
       image: stakeLab,
       title: 'StakeLab',
-      subtitle: 'LIVE',
+      subtitle: 'Live',
       path: 'stakelab'
     },
     {
       image: appLab,
       title: 'AppLab',
-      subtitle: 'LAUNCH IN 2023',
+      subtitle: 'Launch in 2023',
       disabled: true,
     },
     {
       image: dataLab,
       title: 'DataLab',
-      subtitle: 'LAUNCH IN 2024',
+      subtitle: 'Launch in 2024',
       disabled: true,
     },
   ];
@@ -46,38 +46,40 @@ const Navbar = () => {
 
   return (
     <nav className="Navbar">
-      <div className="Navbar__logo--mobile">
-        {/* Mobile */}
-        <Link to={`/`}><Favicon color={false} /></Link>
-      </div>
-      <div className="Navbar__logo--desktop">
-        {/* Desktop */}
-        <Link to={`/`}><Logo color={false} /></Link>
-      </div>
-
-      <div className="Navbar__menu">
-        {/* Mobile */}
-        {/* <div className="Navbar__menu--mobile" onClick={() => setIsMenuOpen(!isMenuOpen)}>
-          { isMenuOpen ? <LogoCross className="icon--small icon--small--cross icon__white" /> : <LogoMenu className="icon--small icon__white" /> }
-        </div> */}
-        <div className={`Navbar__menu--mobile burger ${isMenuOpen ? 'open' : ''}`} onClick={() => setIsMenuOpen(!isMenuOpen)}>
-          <div className="burger-inner">
-            <div></div>
-            <div></div>
-          </div>
+      <div className="Navbar__content">
+        <div className="Navbar__logo--mobile">
+          {/* Mobile */}
+          <Link to={`/`}><Favicon color={false} /></Link>
+        </div>
+        <div className="Navbar__logo--desktop">
+          {/* Desktop */}
+          <Link to={`/`}><Logo color={false} /></Link>
         </div>
 
-        {/* Desktop */}
-        <div className={`Navbar__menu--desktop ${isMenuOpen ? 'open' : ''}`}>
-          <div className="Navbar__menu--desktop__item">
-            <Link to={`#`} className="Navbar__link--white">Products</Link>
-            <span className="Navbar__menu--desktop__item__spacer"></span>
-            <Dropdown items={dropdownItems}/>
+        <div className="Navbar__menu">
+          {/* Mobile */}
+          {/* <div className="Navbar__menu--mobile" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            { isMenuOpen ? <LogoCross className="icon--small icon--small--cross icon__white" /> : <LogoMenu className="icon--small icon__white" /> }
+          </div> */}
+          <div className={`Navbar__menu--mobile burger ${isMenuOpen ? 'open' : ''}`} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <div className="burger-inner">
+              <div></div>
+              <div></div>
+            </div>
           </div>
-          <Link to={`networks`} className="Navbar__link--white">Networks</Link>
-          <Link to={`about`} className="Navbar__link--white">About</Link>
-          <Link to={`faq`} className="Navbar__link--white">FAQ</Link>
-          <button className="btn--small btn__primary"><Link to={`networks`} >Stake</Link></button>
+
+          {/* Desktop */}
+          <div className={`Navbar__menu--desktop ${isMenuOpen ? 'open' : ''}`}>
+            <div className="Navbar__menu--desktop__item">
+              <Link to={`#`} className="Navbar__link--white">Products</Link>
+              <span className="Navbar__menu--desktop__item__spacer"></span>
+              <Dropdown items={dropdownItems}/>
+            </div>
+            <Link to={`networks`} className="Navbar__link--white">Networks</Link>
+            <Link to={`about`} className="Navbar__link--white">About</Link>
+            <Link to={`faq`} className="Navbar__link--white">FAQ</Link>
+            <button className="btn--small btn__primary"><Link to={`networks`} >Stake</Link></button>
+          </div>
         </div>
       </div>
     </nav>
