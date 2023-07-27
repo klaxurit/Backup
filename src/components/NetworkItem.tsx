0e9336706c0ffbca8e26c77f1b61300262461ebb
Axurit <hjunca@skalv-studio.fr>
2023-07-27 16:32:34 +0200

    show tooltip

--- src/components/NetworkItem.tsx
+++ src/components/NetworkItem.tsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { useState } from "react";
 
 import { Network } from "../types";
 
@@ -8,35 +8,49 @@ interface NetworkItemProps {
 }
 
 export const NetworkItem: React.FC<NetworkItemProps> = ({ network, detailed = false }: NetworkItemProps) => {
-  // const [showTooltip, setShowTooltip] = useState(false);
+    const [showTooltip, setShowTooltip] = useState(false);
 
-  // const handleMouseOver = () => {
-  //   setShowTooltip(true);
-  // };
+    const handleMouseOver = () => {
+      if (network.title.length > 11) {
+        setShowTooltip(true);
+      }
+    };
 
-  // const handleMouseOut = () => {
-  //   setShowTooltip(false);
-  // };
+    const handleMouseOut = () => {
+      setShowTooltip(false);
+    };
+
+    const truncateTitle = (title: string) => {
+      if (title.length > 11) {
+        return title.substring(0, 11) + "...";
+      }
+      return title;
+    };
+
+    const displayTitle = truncateTitle(network.title);
 
   if (detailed) {
     return (
       <div 
         className="Network__item Network__item--detailed"
         tabIndex={0}
-        // onMouseOver={handleMouseOver}
-        // onMouseOut={handleMouseOut}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
       >
-        {/* {showTooltip && (
+        {showTooltip && (
           <div className="Network__item__tooltip">
             {network.title}
+            <svg className="Network__item__tooltip__svg" width="12" height="4" viewBox="0 0 12 4" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0 0L12 0L8.4 3.13445C7.07452 4.28852 4.92548 4.28852 3.6 3.13445L0 0Z" fill="white" />
+            </svg>
           </div>
-        )} */}
+        )}
         <div className="Network__item__header">
           <img src={network.imageUrl} className="Network__item__header__img" draggable={false} />
           <div className="Network__item__header__info">
           <div className="Network__item__header__info__head">
             <h3 className="Network__item__header__info__head__title">
-              {network.title}
+              {displayTitle}
             </h3>
             <p className="Network__item__header__info__head__desc">
               {network.description}
