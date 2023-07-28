99b3568bf9f03c6d06ee9668a3a9a4b6c27da6b1
Axurit <hjunca@skalv-studio.fr>
2023-07-28 10:54:11 +0200

    tablet view

--- src/pages/Landing/Hero.tsx
+++ src/pages/Landing/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero: React.FC = () => {
   return (
     <div className="Hero">
       <h1 className="Hero__title">Grow your assets</h1>
-      <p className="Hero__desc">Benefits from StakeLab products while keeping the full ownership of your funds</p>
+      <h5 className="Hero__desc">Benefits from StakeLab products while keeping the full ownership of your funds</h5>
       <div className="Hero__iconWrapper">
         <a href="https://t.me/stakelab_zone" target="_blank"><LogoMail className="Hero__icon icon__grey icon--small" /></a>
         <a href="https://twitter.com/StakeLab" target="_blank"><LogoTwitter className="Hero__icon icon__grey icon--small" /></a>
