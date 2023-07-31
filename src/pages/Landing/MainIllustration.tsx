7a6e23d16dde5c60813991b728a8c65cbf5d37a5
Axurit <hjunca@skalv-studio.fr>
2023-07-31 15:40:45 +0200

    test with react-player

--- src/pages/Landing/MainIllustration.tsx
+++ src/pages/Landing/MainIllustration.tsx
@@ -1,32 +1,28 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
+import ReactPlayer from "react-player";
 import MainIllustrationSVG from "../../assets/images/Logo.mp4";
-import MainIllustrationWEBM from "../../assets/images/Logo.webm";
 import MainIllustrationPNG from "../../assets/images/mainillustration.png";
 
 export const MainIllustration: React.FC = () => {
-  const [videoLoaded, setVideoLoaded] = useState(false);
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  const handleVideoLoad = () => {
-    setVideoLoaded(true);
-    if (videoRef.current) {
-      videoRef.current.play();
-    }
-  };
-
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.load();
-    }
-  }, []);
-
   return (
     <div className="main-illustration">
-      {!videoLoaded && <img src={MainIllustrationPNG} className="main-img" alt="Loading..." />}
-      <video ref={videoRef} playsInline autoPlay loop muted className="main-video" onLoadedData={handleVideoLoad}>
-        <source src={MainIllustrationSVG} type="video/mp4" />
-        <source src={MainIllustrationWEBM} type="video/webm" />
-      </video>
+      <div className="main-video">
+        <ReactPlayer
+          url={MainIllustrationSVG}
+          playing={true}
+          loop={true}
+          muted={true}
+          width="100%"
+          height="100%"
+          config={{
+            file: {
+              attributes: {
+                poster: MainIllustrationPNG,
+              },
+            },
+          }}
+        />
+      </div>
     </div>
   );
-}
+};
