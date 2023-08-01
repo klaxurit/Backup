f8a4551a9851e992fe12f09b4fbc32cd43daced0
Axurit <hjunca@skalv-studio.fr>
2023-08-01 11:40:01 +0200

    try something to autoplay video on safari after loaded

--- src/pages/Landing/Header/MainIllustration.tsx
+++ src/pages/Landing/Header/MainIllustration.tsx
@@ -1,19 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import MainIllustrationSVG from "../../../assets/images/Logo.mp4";
 import MainIllustrationWEBM from "../../../assets/images/Logo.webm";
-import MainIllustrationPNG from "../../../assets/images/mainillustration.png"; 
+import MainIllustrationPNG from "../../../assets/images/mainillustration.png";
 
 export const MainIllustration: React.FC = () => {
   const [videoLoaded, setVideoLoaded] = useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
 
   const handleVideoLoad = () => {
     setVideoLoaded(true);
   };
 
   useEffect(() => {
-    if (document.querySelector('.main-video')) {
-      document.querySelector('.main-video')!.addEventListener('canplay', handleVideoLoad);
+    if (videoRef.current) {
+      videoRef.current.addEventListener('canplay', handleVideoLoad);
+
+      const playPromise = videoRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise
+          .then(() => {
+          })
+          .catch(error => {
+          });
+      }
     }
+
+    return () => {
+      if (videoRef.current) {
+        videoRef.current.removeEventListener('canplay', handleVideoLoad);
+      }
+    };
   }, []);
 
   return (
@@ -21,7 +37,7 @@ export const MainIllustration: React.FC = () => {
       {!videoLoaded &&
         <img className="main-img" src={MainIllustrationPNG} alt="Placeholder" />
       }
-      <video onCanPlay={handleVideoLoad} playsInline autoPlay loop muted className="main-video" preload="metadata" src={MainIllustrationWEBM}>
+      <video ref={videoRef} onCanPlay={handleVideoLoad} playsInline autoPlay loop muted className="main-video" preload="metadata" src={MainIllustrationWEBM}>
         <source src={MainIllustrationSVG} type="video/mp4" />
         <source src={MainIllustrationWEBM} type="video/webm" />
       </video>
