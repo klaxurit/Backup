33efe29782b2e0d56cba665ae548511ea494de63
Axurit <hjunca@skalv-studio.fr>
2023-08-01 10:18:46 +0200

    clean code

--- src/pages/Landing/MainIllustration.tsx
+++ src/pages/Landing/MainIllustration.tsx
@@ -1,28 +1,11 @@
-import React, { useState, useRef, useEffect } from "react";
+import React from "react";
 import MainIllustrationSVG from "../../assets/images/Logo.mp4";
 import MainIllustrationWEBM from "../../assets/images/Logo.webm";
-import MainIllustrationPNG from "../../assets/images/mainillustration.png";
 
 export const MainIllustration: React.FC = () => {
-  // const [videoLoaded, setVideoLoaded] = useState(false);
-  // const videoRef = useRef<HTMLVideoElement>(null);
-
-  // const handleVideoLoad = () => {
-  //   setVideoLoaded(true);
-  //   if (videoRef.current) {
-  //     videoRef.current.play();
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   if (videoRef.current) {
-  //     videoRef.current.load();
-  //   }
-  // }, []);
 
   return (
     <div className="main-illustration">
-      {/* {!videoLoaded && <img src={MainIllustrationPNG} className="main-img" alt="Loading..." />} */}
       <video playsInline autoPlay loop muted className="main-video" preload="metadata" src={MainIllustrationWEBM}>
         <source src={MainIllustrationSVG} type="video/mp4" />
         <source src={MainIllustrationWEBM} type="video/webm" />
