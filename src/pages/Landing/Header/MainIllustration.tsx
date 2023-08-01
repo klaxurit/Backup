4b474aa7d7b08b8999add03690da232367aaf7f8
Axurit <hjunca@skalv-studio.fr>
2023-08-01 16:51:34 +0200

    rework video convertion and play different video on mobile/desktop

--- src/pages/Landing/Header/MainIllustration.tsx
+++ src/pages/Landing/Header/MainIllustration.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+
+import useMediaQuery from "../../../hooks/useMediaQuery";
 import MainIllustrationSVG from "../../../assets/images/Logo.mp4";
+import MainIllustrationSVGMobile from "../../../assets/images/Logo_Mobile.mp4";
 import MainIllustrationWEBM from "../../../assets/images/Logo.webm";
+import MainIllustrationWEBMMobile from "../../../assets/images/Logo_Mobile.webm";
 import MainIllustrationPNG from "../../../assets/images/mainillustration2.png";
 
 export const MainIllustration: React.FC = () => {
-
+  const isMobile = useMediaQuery('(max-width:992px)');
   return (
     <div className="main-illustration">
       <video playsInline autoPlay loop muted className="main-video" poster={MainIllustrationPNG}>
-        <source src={MainIllustrationSVG} type="video/mp4" />
-        <source src={MainIllustrationWEBM} type="video/webm" />
+        <source src={isMobile ? MainIllustrationSVGMobile : MainIllustrationSVG} type="video/mp4" />
+        <source src={isMobile ? MainIllustrationWEBMMobile : MainIllustrationWEBM} type="video/webm" />
         <img className="main-img" src={MainIllustrationPNG} alt="Placeholder" />
       </video>
     </div>
